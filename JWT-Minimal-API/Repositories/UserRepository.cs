c487dd03addcc209d710b85e3c4f55a2a4e9155c
bartdeveloper <bartek@appselio.pl>
2022-01-25 15:51:48 +0100

    Added user repository

---
+++ JWT-Minimal-API/Repositories/UserRepository.cs
@@ -0,0 +1,13 @@
+﻿using JWT_Minimal_API.Models;
+
+namespace JWT_Minimal_API.Repositories
+{
+    public class UserRepository
+    {
+        public static List<User> Users = new()
+        {
+            new() { Username = "admin1", EmailAddress = "test.admin@mail.com", Password = "Pa$$w0rd", GivenName = "Bart", Surname = "Adminer", Role = "Administrator" },
+            new() { Username = "normal1", EmailAddress = "test.normal@mail.com", Password = "Pa$$w0rd", GivenName = "Normie", Surname = "Smith", Role = "Normal" },
+        };
+    }
+}
